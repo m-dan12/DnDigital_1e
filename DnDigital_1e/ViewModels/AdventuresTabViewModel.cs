0e56dfc9ad6016e1072d0401c687dd8d2fa5cc13
Liakitori <liakitori@mail.ru>
2024-04-02 23:57:11 +0400

    Изменил MainWindowViewModel, добавив класс SubMenu для будущих элементов левого меню. Начал работу в AdvenuresTab (Заметки)

---
+++ DnDigital_1e/ViewModels/AdventuresTabViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+
+namespace DnDigital_1e.ViewModels
+{
+    public class AdventuresTabViewModel : MainWindowViewModel, INotifyPropertyChanged
+    {
+
+        public AdventuresTabViewModel()
+        {
+
+        }
+    }
+}
--- DnDigital_1e/ViewModels/MainWindowViewModel.cs
+++ DnDigital_1e/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,42 @@
 ﻿using DnDigital_1e.Views;
 using System.ComponentModel;
+using System.Data.Common;
+using System.Reactive.Linq;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls;
 
 namespace DnDigital_1e.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
     {
-
+        public class SubmenuItem : ReactiveObject
+        {
+            private string _name;
+            private string _icon;
+            public string Name
+            {
+                get => _name;
+                set => this.RaiseAndSetIfChanged(ref _name, value);
+            }
+            public string Icon
+            {
+                get => _icon;
+                set => this.RaiseAndSetIfChanged(ref _icon, value);
+            }
+            public SubmenuItem(string name, string icon)
+            {
+                _name = name;
+                _icon = icon;
+            }
+        }
+        public ObservableCollection<SubmenuItem> SubmenuItems { get; set; } = [];
+        public MainWindowViewModel()
+        {
+            
+        }
     }
 }
