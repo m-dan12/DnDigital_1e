880c5ded55a9efa35f45bdef1e8891fbf21e0d1d
Даниил Малинин <m-dan12@ya.ru>
2024-04-04 13:36:27 +0400

    Сделал кнопки скрыть, развернуть/свернуть/ закрыть

--- DnDigital_1e/ViewModels/MainWindowViewModel.cs
+++ DnDigital_1e/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
-﻿using DnDigital_1e.Views;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Data.Common;
 using System.Reactive.Linq;
 using ReactiveUI;
@@ -8,6 +7,11 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls;
+using ReactiveUI;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using System.Reactive;
+using Avalonia.Input;
 
 namespace DnDigital_1e.ViewModels
 {
@@ -38,5 +42,61 @@ namespace DnDigital_1e.ViewModels
         {
             
         }
+    }
+}
+
+
+
+        #region Объявление команд
+
+        private ReactiveCommand<Unit, Unit> _closeCommand;
+        private ReactiveCommand<Unit, Unit> _hideCommand;
+        private ReactiveCommand<Unit, Unit> _deployCommand;
+
+        #endregion
+
+
+
+        #region Инициализация команд
+
+        public ReactiveCommand<Unit, Unit> CloseCommand =>
+            _closeCommand ?? (_closeCommand = ReactiveCommand.Create(CloseWindow));
+        public ReactiveCommand<Unit, Unit> HideCommand =>
+            _hideCommand ?? (_hideCommand = ReactiveCommand.Create(HideWindow));
+        public ReactiveCommand<Unit, Unit> DeployCommand =>
+            _deployCommand ?? (_deployCommand = ReactiveCommand.Create(DeployWindow));
+
+        #endregion
+
+
+
+        #region Методы
+
+        private void CloseWindow()
+        {
+            var window = (App.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
+            window.Close();
+        }
+        private void HideWindow()
+        {
+            var window = (App.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
+            window.WindowState = WindowState.Minimized;
+        }
+        private void DeployWindow()
+        {
+            var window = (App.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
+            window.WindowState = window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        #endregion
+
+
+
+        // Реализация интерфейса INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+
     }
 }
